1316d32d7723dc9932c6a26b90fb39ded7441756
morswin22 <xmorswinx@gmail.com>
2020-09-19 12:13:36 +0200

    :sparkles: Add polish number-plate font

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import cv2 as cv
 import numpy as np
 import string
+from PIL import ImageFont, ImageDraw, Image
 from random import uniform, random, choices
+
+width = 250
+height = 100
 
 diff = 30
 angle = 30
 letters = 5
 
+font_size = 1
+
+font = ImageFont.truetype('polish.ttf', font_size)
+while font.getsize('W'*letters)[0] < width:
+  font_size += 1
+  font = ImageFont.truetype('polish.ttf', font_size)
+
+text_offset = (height - font.getsize('A')[1]) / 2
+
 while True:
   text = ''.join(choices(string.ascii_uppercase + string.digits, k=letters))
 
-  src = 255 * np.ones(shape=[75, 150, 3], dtype=np.uint8)
+  src = 255 * np.ones(shape=[height, width, 3], dtype=np.uint8)
 
-  cv.putText(src, text, (0, 55), cv.FONT_HERSHEY_PLAIN, min(src.shape[0],src.shape[1])/25, 0, thickness=3);
+  src_pil = Image.fromarray(src)
+  draw = ImageDraw.Draw(src_pil)
+  draw.text((0, text_offset), text, font=font, fill=(0, 0, 0, 0))
+  src = np.array(src_pil)
 
   srcTri = np.array( [[0, 0], [src.shape[1] - 1, 0], [0, src.shape[0] - 1]] ).astype(np.float32)
   # TODO Fix dstTri sometimes overflowing boundaries
