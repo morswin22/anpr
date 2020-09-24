f9799478d6feef5f39196029a5851bff58a07944
morswin22 <xmorswinx@gmail.com>
2020-09-24 22:23:39 +0200

    :sparkles: Render and save CNN training input

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
-# pylint: disable=maybe-no-member
+import json
+import os
+import string
+from random import choice, choices, random, uniform
+
 import cv2 as cv
 import numpy as np
-import string
-from PIL import ImageFont, ImageDraw, Image
-from random import uniform, random, choices, choice
-import json
 import tensorflow_datasets as tfds
+from PIL import Image, ImageDraw, ImageFont
 
-n_backgrounds = 100
-padding = 50
+save_path = 'generated'
+generate = 1000
+
+padding = 70, 120
+delta = 50
 width = 520
 height = 114
-angle = 40
+angle = 35
+# TODO Add varying number of letters
 letters = 7
 aside = 45
+final_size = 128, 64
 
-ds, ds_info = tfds.load("sun397/standard-part1-120k", split='train', shuffle_files=True, with_info=True)
-tfds.show_examples(ds, ds_info)
+if os.path.exists(save_path):
+  print(f'Remove {save_path}/ to continue')
+  exit()
 
-backgrounds = []
-for example in ds.take(n_backgrounds):
-  backgrounds.append(example['image'])
+os.makedirs(save_path)
 
-font_size = 1
-
-font = ImageFont.truetype('polish.ttf', font_size)
-while font.getsize(' '+'E'*letters)[0] < width - aside:
-  font_size += 1
-  font = ImageFont.truetype('polish.ttf', font_size)
-
-text_offset = -5 + (height - font.getsize('A')[1]) / 2
+ds = tfds.load("sun397/standard-part1-120k", split='train+test', shuffle_files=True)
 
 with open('numbers.json', 'r') as file:
   numbers = json.load(file)
@@ -38,25 +36,35 @@ uppercase = string.ascii_uppercase
 for letter in numbers['removed']: 
   uppercase = uppercase.replace(letter, '') 
 
-for background in backgrounds:
+font_size = 1
+font = ImageFont.truetype('polish.ttf', font_size)
+while font.getsize(' '+'E'*letters)[0] < width - aside:
+  font_size += 1
+  font = ImageFont.truetype('polish.ttf', font_size)
+text_offset = -5 + (height - font.getsize('A')[1]) / 2
+
+path = save_path + '/gen_'
+for i, example in enumerate(ds.take(generate)):
+  background = example['image']
+
   county = choice(numbers['counties'])
   text = county + ' ' + ''.join(choices(uppercase + string.digits, k=letters-len(county)))
 
-  src = np.zeros(shape=[height + 2*padding, width + 2*padding, 4], dtype=np.uint8)
+  src = np.zeros(shape=[height + 2*padding[1], width + 2*padding[0], 4], dtype=np.uint8)
 
   src_pil = Image.fromarray(src)
   draw = ImageDraw.Draw(src_pil)
-  draw.rectangle([(padding, padding), (width+padding, height+padding)], fill=(255, 255, 255, 255))
-  draw.text((padding + aside, padding + text_offset), text, font=font, fill=(0, 0, 0, 255))
+  draw.rectangle([padding, (width+padding[0], height+padding[1])], fill=(255, 255, 255, 255))
+  draw.text((padding[0] + aside, padding[1] + text_offset), text, font=font, fill=(0, 0, 0, 255))
   src = np.array(src_pil)
 
   srcTri = np.array( [[0, 0], 
                       [src.shape[1] - 1, 0], 
                       [0, src.shape[0] - 1]] ).astype(np.float32)
 
-  dstTri = np.array( [[random()*padding, random()*padding],
-                      [src.shape[1]-random()*padding, random()*padding], 
-                      [random()*padding, src.shape[0]-random()*padding]] ).astype(np.float32)
+  dstTri = np.array( [[random()*delta, random()*delta],
+                      [src.shape[1]-random()*delta, random()*delta], 
+                      [random()*delta, src.shape[0]-random()*delta]] ).astype(np.float32)
 
   warp_mat = cv.getAffineTransform(srcTri, dstTri)
   warp_dst = cv.warpAffine(src, warp_mat, (src.shape[1], src.shape[0]))
@@ -65,14 +73,10 @@ for background in backgrounds:
   transformed = cv.warpAffine(warp_dst, rot_mat, (warp_dst.shape[1], warp_dst.shape[0]))
 
   transformed_pil = Image.fromarray(transformed)
-  bg_pil = Image.fromarray(np.array(background))
-  bg_pil = bg_pil.resize((transformed.shape[1], transformed.shape[0]))
+  ratio = background.shape[1] / background.shape[0]
+  bg_pil = Image.fromarray(np.array(background[...,::-1])).resize((transformed.shape[1], int(transformed.shape[1] / min(ratio, 2))))
   bg_pil.paste(transformed_pil, box=(0,0), mask=transformed_pil)
-  bg = np.array(bg_pil)
+  bg = np.array(bg_pil.crop(box=(0, 0, transformed.shape[1], transformed.shape[1] // 2)).resize(final_size))
 
-  cv.imshow('Number-plate', src)
-  cv.imshow('Transformed', transformed)
-  cv.imshow('Final', bg)
-
-  if cv.waitKey() == 27:
-    break
+  # TODO Create and store CNN training output
+  cv.imwrite(path + str(i) + '.png', bg)
