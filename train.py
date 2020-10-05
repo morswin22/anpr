cf7cd410ac7c860ea6e177849c4bae6309d63d76
morswin22 <xmorswinx@gmail.com>
2020-10-05 19:42:51 +0200

    :sparkles: Add checkpoints, tensorboard, optimize memory, increase model size

--- train.py
+++ train.py
@@ -1,16 +1,17 @@
+import argparse
 import json
 import os
+import pathlib
 import re
 from datetime import datetime
-import argparse
 from random import shuffle
 
-import matplotlib.pyplot as plt
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
 import numpy as np
-import tensorflow as tf
 from PIL import Image
-from tensorflow.keras import layers, models
-from tqdm import tqdm
+from tensorflow.keras import callbacks, layers, models
+from tqdm import tqdm, trange
 
 parser = argparse.ArgumentParser(description='Train the CNN model')
 parser.add_argument('-s', '--splits', dest='splits', action='store', default='1', help='number of splits')
@@ -18,6 +19,7 @@ parser.add_argument('-e', '--epochs', dest='epochs', action='store', default='10
 parser.add_argument('-t', '--train', dest='train', action='store', default='0.8', help='percentage of training data in dataset')
 parser.add_argument('-d', '--dir', dest='dir', action='store', default='generated-1-10-397000', help='dataset path')
 parser.add_argument('-m', '--model', dest='model', action='store', default=None, help='path of the model to use')
+parser.add_argument('-c', '--checkpoint', dest='checkpoint', action='store', default=None, help='path of the weights checkpoint to use')
 
 with open('assets/map.json', 'r') as file:
   mapped = json.load(file)
@@ -48,29 +50,35 @@ if not os.path.isdir(dataset_path):
   exit()
 dataset_files = list(os.scandir(dataset_path))
 shuffle(dataset_files)
+dataset_length = len(dataset_files) // n_splits
+split = int(dataset_length * train_with)
+pattern = re.compile("\d+_(.+)\.png")
 
 if arguments.model is None:
   model = models.Sequential()
 
-  model.add(layers.Conv2D(32, (5, 5), activation='relu', input_shape=(128, 64, 1)))
+  model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(128, 64, 1)))
   model.add(layers.BatchNormalization())
   model.add(layers.MaxPool2D((2, 2)))
   model.add(layers.Dropout(0.2))
 
-  model.add(layers.Conv2D(64, (5, 5), activation='relu'))
+  model.add(layers.Conv2D(128, (3, 3), activation='relu'))
   model.add(layers.BatchNormalization())
-  model.add(layers.MaxPool2D((1, 2)))
+  model.add(layers.MaxPool2D((2, 1)))
   model.add(layers.Dropout(0.2))
 
-  model.add(layers.Conv2D(64, (5, 5), activation='relu'))
+  model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+  model.add(layers.BatchNormalization())
+  model.add(layers.MaxPool2D((2, 2)))
+  model.add(layers.Dropout(0.2))
+
+  model.add(layers.Conv2D(512, (3, 3), activation='relu'))
   model.add(layers.BatchNormalization())
   model.add(layers.MaxPool2D((2, 2)))
   model.add(layers.Dropout(0.2))
 
   model.add(layers.Flatten())
-  model.add(layers.Dense(1024, activation='relu'))
-  model.add(layers.Dropout(0.3))
-  model.add(layers.Dense(512, activation='relu'))
+  model.add(layers.Dense(4096, activation='relu'))
   model.add(layers.Dropout(0.3))
 
   model.add(layers.Dense(207, activation='sigmoid'))
@@ -78,55 +86,40 @@ else:
   model = models.load_model(arguments.model)
 model.summary()
 
+if arguments.checkpoint:
+  if os.path.isfile(arguments.checkpoint + 'model.index'):
+    model.load_weights(arguments.checkpoint + 'model')
+  else:
+    print(f'Checkpoint was not found at {arguments.checkpoint}model')
+    exit()
+
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-history = { 'loss': [], 'accuracy': [], 'val_accuracy': [], 'acc': [] }
+name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+tensorboard = callbacks.TensorBoard(os.path.join(pathlib.Path().absolute(), 'logs', name), histogram_freq=1)
+checkpoint = callbacks.ModelCheckpoint(os.path.join(pathlib.Path().absolute(), 'checkpoints', name, 'model'), save_best_only=True, save_weights_only=True)
 
-for i in tqdm(range(n_splits), total=n_splits, desc='Splits', unit='split'):
-  train_images, train_labels, test_images, test_labels = [None] * 4
-  files = dataset_files[i::n_splits]
-  dataset_length = len(files)
-  split = int(dataset_length * train_with)
-
-  pattern = re.compile("\d+_(.+)\.png")
+for i in trange(n_splits, total=n_splits, desc='Splits', unit='split'):
+  train_images, train_labels, test_images, test_labels, result = [None] * 5
   ds_data = []
   ds_labels = []
-  for path in tqdm(files, unit='examples', total=dataset_length):
-    result = pattern.search(path.name)
-    label = encoder(result.group(1))
-    ds_labels.append(label)
-    image = np.array(Image.open(os.path.join(dataset_path, path.name)), dtype=np.uint8).reshape((128, 64, 1)) / 255
-    ds_data.append(image)
+  
+  files = dataset_files[i::n_splits]
+  for file in tqdm(files, unit='examples', total=dataset_length):
+    ds_labels.append(encoder(pattern.search(file.name).group(1)))
+    ds_data.append(np.array(Image.open(file.path), dtype=np.uint8).reshape((128, 64, 1)) / 255)
 
+  files = None
   (train_images, train_labels), (test_images, test_labels) = ((np.array(ds_data[:split]), np.array(ds_labels[:split])), (np.array(ds_data[split:]), np.array(ds_labels[split:])))
+  ds_data, ds_labels = None, None
 
-  result = model.fit(train_images, train_labels, epochs=n_epochs, validation_data=(test_images, test_labels))
-  history['loss'] += result.history['loss']
-  history['accuracy'] += result.history['accuracy']
-  history['val_accuracy'] += result.history['val_accuracy']
-
-  test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
-  history['acc'].append(test_acc)
-
-plt.plot(history['loss'], label='loss')
-plt.plot(history['accuracy'], label='accuracy')
-plt.plot(history['val_accuracy'], label='validation accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0, 1])
-plt.legend(loc='lower right')
-
-print('Accuracy across splits')
-for i, accuracy in enumerate(history['acc']):
-  print(f'#{i+1} accuracy {accuracy}')
+  model.fit(train_images, train_labels, epochs=n_epochs, validation_data=(test_images, test_labels), callbacks=[tensorboard, checkpoint])
+  model.evaluate(test_images, test_labels)
 
 os.makedirs('models', exist_ok=True)
-name = f'models/{str(datetime.now())} model.h5'.replace(':','-').replace(' ','_')
-model.save(name)
+model.save(f"models/{name}_model.h5")
 
 image = np.array(Image.open('assets/test.png'), dtype=np.uint8).reshape((128, 64, 1)) / 255
 predictions = model.predict(np.array([image]))
 print('Ground truth: 125WE 295GC')
 print(f'Prediction: {decoder(predictions[0])}')
-
-plt.show()
